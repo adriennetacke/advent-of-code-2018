c1e77675a61179284f98c4cf95927054f33b1280
Adrienne Tacke <adrienne.tacke@gmail.com>
2018-12-13 22:32:56 -0800

    WIP: day seven part one implementation

--- day-7-the-sum-of-its-parts/steps.js
+++ day-7-the-sum-of-its-parts/steps.js
@@ -2,23 +2,88 @@ const steps = (input) => {
   let steps = input.split('\n');
 
   let finalOrder = [];
+  let allSteps = [];
 
+  // Prepare parent-child relationships
   for (let i = 0; i < steps.length; i++) {
-    let step = steps[i];
-    let matchGroups = step.match(/(\w+) (.) (\w+) (\w+) (\w+) (\w+) (\w+) (.)/);
+    const step = steps[i];
+    const matchGroups = step.match(/(\w+) (.) (\w+) (\w+) (\w+) (\w+) (\w+) (.)/);
 
-    let firstStep = matchGroups[2];
-    let secondStep = matchGroups[8];
+    const parent = matchGroups[2];
+    const child = matchGroups[8];
 
-    // Check if already exists.
-    // If not, add it
-    if (!finalOrder.includes(firstStep)) {
-      finalOrder.push(firstStep);
+    if (!allSteps.find(x => x.step === parent)) {
+      allSteps.push({
+        step: parent,
+        children: [],
+        parents: []
+      });
+    };
+
+    if (!allSteps.find(x => x.step === child)) {
+      allSteps.push({
+        step: child,
+        children: [],
+        parents: []
+      });
+    };
+
+    const parentRelationship = allSteps.find(x => x.step === parent);
+
+    if (!parentRelationship.children.includes(child)) {
+      parentRelationship.children.push(child);
+    };
+
+    const childRelationship = allSteps.find(x => x.step === child);
+
+    if(!childRelationship.parents.includes(parent)) {
+      childRelationship.parents.push(parent);
     }
-
-    // Sort second step alphabetically
-    // then add
   }
+
+  // Start with parent-less steps
+  const orphanedCandidates = allSteps.filter(x => !x.parents.length);
+  let stagedSteps = new Set();
+
+  if (orphanedCandidates.length) {
+    orphanedCandidates.map(x => {
+      stagedSteps.add(x.step);
+    });
+  }
+
+  console.log(stagedSteps);
+
+  while (true) {
+    const sortedStagedSteps = [...stagedSteps].sort();
+    
+    let i = 0;
+    let potentialStep = allSteps.find(s => s.step === sortedStagedSteps[i]);
+    
+    if (!potentialStep.parents.length || potentialStep.parents.every(p => sortedStagedSteps.includes(x))) {
+      finalOrder.push(potentialStep.step);
+      stagedSteps.add(potentialStep.children);
+      stagedSteps.delete(potentialStep.step);
+      break;
+    } else if (potentialStep.parents.every(p => sortedStagedSteps.includes(x))) {
+      finalOrder.push(potentialStep.step);
+    };
+    
+    i++;
+
+  }
+
+
+
+  // Check for no parents or available steps
+    // Add all candidates to watchlist
+    // If more than one, sort alphabetically 
+    // Find first in alphabet, then add to final order
+  // Find children
+    // Add children to watchlist that don't exist yet
+  // Go through new watchlist alphabetically to find next likely step
+    // Find candidate's required parents
+    // Compare all required parents to available steps in watchlist
+  // Add next step
 };
 
 module.exports = steps;
